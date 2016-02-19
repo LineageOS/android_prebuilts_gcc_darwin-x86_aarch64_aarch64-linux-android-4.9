e5dd6d374a4ec26d9fec0673dc3e12c96cd4995c
Than McIntosh <thanm@google.com>
2016-02-19 09:14:37 -0500

    Update prebuilt GCC to build 2617618.
    Highlights
     * gold linker fix for the noisy A53 errata messages
     * gold linker fix for huge arm32 PLT overflow bug (PR gold/18780)
    
    Bug: http://b/26536732
    
    Change-Id: Ic55ee76598989cd47732f27a4045a128b6aa9143

--- lib/gcc/aarch64-linux-android/4.9/include-fixed/linux/a.out.h
+++ lib/gcc/aarch64-linux-android/4.9/include-fixed/linux/a.out.h
@@ -2,7 +2,7 @@
 
     It has been auto-edited by fixincludes from:
 
-	"/tmp/43dcb2459a54a7a9387d188b74433069/sysroot/usr/include/linux/a.out.h"
+	"/tmp/5f456d13928ae03aa93ca31fef092e19/sysroot/usr/include/linux/a.out.h"
 
     This had to be done to correct non-standard usages in the
     original, manufacturer supplied header file.  */
